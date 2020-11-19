74fbd6d3b8b718d043260e227fa400b0f4b50019
Sled <angelherxd@hotmail.com>
2020-11-18 20:33:45 -0700

    some minor changes

--- web/mobile/interface.js
+++ web/mobile/interface.js
@@ -1,7 +1,10 @@
 //HTML functions
 window.onload = function () {
-	var weekTemp = "Weekly temperature: 27 C";
+	var weekTemp = "At optimal value";
 	var dailyTemp = "Actual temperature: 24 C";
 
 	document.getElementById("waterLevel").innerText = weekTemp;
+	$('#loadingModal').modal({
+		show: true
+	})
 };
