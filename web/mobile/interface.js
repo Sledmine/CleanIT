a9efc874bd4d202f49655e7bb0f29ba2e53b47f9
Sled <angelherxd@hotmail.com>
2020-11-25 18:54:40 -0700

    fixes for the ui

--- web/mobile/interface.js
+++ web/mobile/interface.js
@@ -1,10 +1,10 @@
 //HTML functions
 window.onload = function () {
-	var weekTemp = "At optimal value";
+	var weekTemp = "Optimo";
 	var dailyTemp = "Actual temperature: 24 C";
 
 	document.getElementById("waterLevel").innerText = weekTemp;
 	$('#loadingModal').modal({
-		show: true
+		show: false
 	})
 };
